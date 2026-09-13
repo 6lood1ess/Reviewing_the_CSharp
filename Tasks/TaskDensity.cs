4a3f517e940e677222c1a7d4d7fcb2a560e931df
6lood1ess <6lo.od1ess@gmail.com>
2026-09-13 19:32:28 +0700

    The static class Task8_Density is implemented in the Tasks folder. This file contains the mathematical logic for solving task No. 1 (the first image). The CalculateDensity method takes the geometric parameters of the part and its mass, calculates the volume using the area of a right‑angled triangle, and returns the material density

---
+++ Tasks/TaskDensity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Reviewing_the_CSharp.Tasks {
+  public static class TaskDensity {
+
+    // Вычисляет плотность материала детали
+    public static double CalculateDensity(double a, double b, double h, double m) {
+      // Первый катет a (см) должен быть > 0
+      if (a <= 0) throw new ArgumentException("Катет a должен быть положительным", nameof(a));
+
+      // Второй катет b (см) должен быть > 0
+      if (b <= 0) throw new ArgumentException("Катет b должен быть положительным", nameof(b));
+
+      // Толщина h (см) должна быть > 0
+      if (h <= 0) throw new ArgumentException("Толщина h должна быть положительной", nameof(h));
+
+      // Масса m (г) должна быть > 0
+      if (m <= 0) throw new ArgumentException("Масса m должна быть положительной", nameof(m));
+
+      // Площадь прямоугольного треугольника = (a * b) / 2
+      double area = (a * b) / 2.0;
+
+      // Объем детали = площадь * толщина
+      double volume = area * h;
+
+      // Плотность (г/см^3) = масса / объем
+      return m / volume;
+    }
+  }
+}
