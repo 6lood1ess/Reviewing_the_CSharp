1213a96b60f8f884eb5e3f31915fe4c54cfb83af
6lood1ess <6lo.od1ess@gmail.com>
2026-09-13 19:50:01 +0700

    The Task8_PhoneBook class has been created in the Tasks folder. This module is responsible for solving problem No. 2 (second picture). The FilterSubscribers method takes a list of Subscriber objects, analyzes strings of phone numbers and area codes, extracts the last two digits (suffixes yy and бб) and returns a filtered list of those subscribers for whom these numbers match

---
+++ Tasks/TaskPhoneBook.cs
@@ -0,0 +1,28 @@
+﻿using Reviewing_the_CSharp.Models;
+
+namespace Reviewing_the_CSharp.Tasks {
+  public static class Task8_PhoneBook {
+    // Фильтрует список абонентов, у которых код города (бб) совпадает с кодом номера (уу)
+    public static List<Subscriber> FilterSubscribers(List<Subscriber> subscribers) {
+      var result = new List<Subscriber>();
+
+      if (subscribers == null) {
+        return result;
+      }
+
+      foreach (var sub in subscribers) {
+        // Извлечение последних 2 символов из номера телефона (уу)
+        string phoneSuffix = sub.PhoneNumber.Substring(sub.PhoneNumber.Length - 2);
+
+        // Извлечение последних 2 символов из кода города (бб)
+        string areaSuffix = sub.AreaCode.Substring(sub.AreaCode.Length - 2);
+
+        if (phoneSuffix == areaSuffix) {
+          result.Add(sub);
+        }
+      }
+
+      return result;
+    }
+  }
+}
