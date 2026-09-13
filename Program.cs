2c8a0e78e243d23e115fe4aee285362c56d116a0
6lood1ess <6lo.od1ess@gmail.com>
2026-09-13 21:58:18 +0700

    The ConsoleManager has a new method, PrintTaskConditions, which displays the conditions for tasks 1 and 2. In the Program menu, this method is called after entering the number 3

--- Program.cs
+++ Program.cs
@@ -8,33 +8,58 @@ namespace Reviewing_the_CSharp {
       var taskDensity = new TaskDensity();
       var taskPhoneBook = new TaskPhoneBook();
 
-      Console.WriteLine("Выберите задачу для выполнения:" +
+      bool isRunning = true;
+
+      while (isRunning) {
+        Console.Clear();
+        Console.WriteLine("Выберите задачу для выполнения:" +
                         "\n1 - Задача 1: плотность детали" +
-                        "\n2 - Задача 2: телефонный справочник");
-      
-      Console.Write("\nВаш выбор: ");
-      string choice = Console.ReadLine();
+                        "\n2 - Задача 2: телефонный справочник" +
+                        "\n3 - Ознакомиться с условиями задач" +
+                        "\n4 - Выйти из программы");
 
-      switch (choice) {
-        case "1":
-          var (a, b, h, m) = consoleManager.GetDensityInput();
-          double density = taskDensity.CalculateDensity(a, b, h, m);
-          consoleManager.PrintDensityResult(density);
-          break;
+        Console.Write("\nВаш выбор: ");
+        string choice = Console.ReadLine();
 
-        case "2":
-          var subscribers = consoleManager.GetSubscribersInput();
-          var filtered = taskPhoneBook.FilterSubscribers(subscribers);
-          consoleManager.PrintSubscribers(filtered);
-          break;
+        switch (choice) {
 
-        default:
-          Console.WriteLine("Неверный выбор");
-          break;
+          case "1":
+            var (a, b, h, m) = consoleManager.GetDensityInput();
+            double density = taskDensity.CalculateDensity(a, b, h, m);
+            consoleManager.PrintDensityResult(density);
+
+            Console.WriteLine("\nНажмите любую клавишу для выхода в меню...");
+            Console.ReadKey();
+            break;
+
+          case "2":
+            var subscribers = consoleManager.GetSubscribersInput();
+            var filtered = taskPhoneBook.FilterSubscribers(subscribers);
+            consoleManager.PrintSubscribers(filtered);
+
+            Console.WriteLine("\nНажмите любую клавишу для выхода в меню...");
+            Console.ReadKey();
+            break;
+
+          case "3":
+            consoleManager.PrintTaskConditions();
+
+            Console.WriteLine("\nНажмите любую клавишу для выхода в меню...");
+            Console.ReadKey();
+            break;
+
+          case "4":
+            isRunning = false;
+            break;
+
+          default:
+            Console.WriteLine("\nНеверный выбор");
+
+            Console.WriteLine("\nНажмите любую клавишу для выхода в меню...");
+            Console.ReadKey();
+            break;
+        }
       }
-
-      Console.WriteLine("\nНажмите любую клавишу для выхода...");
-      Console.ReadKey();
     }
   }
 }
