b99a6c1fe7a8a157453c9fbbd90e4b3b4c76efab
6lood1ess <6lo.od1ess@gmail.com>
2026-09-13 19:12:49 +0700

    The Subscriber class has been added to the Models folder. This file describes the data structure for storing information about a subscriber (last name, phone number, and area code)

---
+++ Models/Subscriber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Reviewing_the_CSharp.Models {
+  public class Subscriber {
+    public string LastName { get; }
+    public string PhoneNumber { get; } // Формат xxуу (например, 1234)
+    public string AreaCode { get; } // Формат аабб (например, 5678)
+
+    int permissibleLengthOfAPhoneNumber = 4;
+    int permissibleLengthOfTheAreaCode = 4;
+
+    public Subscriber(string lastName, string phoneNumber, string areaCode) {
+      if (string.IsNullOrWhiteSpace(lastName)) {
+        throw new ArgumentException("Фамилия не может быть пустой", nameof(lastName));
+      }
+
+      if (!IsDigitsOnly(phoneNumber) || phoneNumber.Length != permissibleLengthOfAPhoneNumber) {
+        throw new ArgumentException("Номер телефона должен состоять ровно из 4 цифр (формат xxуу)", nameof(phoneNumber));
+      }
+
+      if (!IsDigitsOnly(areaCode) || areaCode.Length != permissibleLengthOfTheAreaCode) {
+        throw new ArgumentException("Код города должен состоять ровно из 4 цифр (формат аабб)", nameof(areaCode));
+      }
+
+      LastName = lastName.Trim();
+      PhoneNumber = phoneNumber;
+      AreaCode = areaCode;
+    }
+
+    private static bool IsDigitsOnly(string textToCheck) {
+      if (string.IsNullOrEmpty(textToCheck)) {
+        return false;
+      }
+
+      foreach (char currentCharacter in textToCheck) {
+        if (!char.IsDigit(currentCharacter)) {
+          return false;
+        }
+      }
+
+      return true;
+    }
+  }
+}
